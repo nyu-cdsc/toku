9081e7514c2e26d4220daf0c3003db4909c2ea31
Kelsey Moty <7917220+kelseymoty@users.noreply.github.com>
2018-03-27 12:04:19 -0400

    added more animal stimuli

--- src/app/stimuli/default-stimuli.ts
+++ src/app/stimuli/default-stimuli.ts
@@ -31,6 +31,14 @@ export const STUDIES: Study[] = [
                         picture: ["../assets/image/kangaroos.jpg"],
                         sound: ["../assets/audio/kg_1.m4a"]
                     },
+                    {
+                        id: "skunk", 
+                        picture1: [""],
+                        sound1: [""],
+                        movie: ["../assets/video/skintro1.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_1.m4a"]
+                    },
                 ]
             },
 
@@ -38,28 +46,36 @@ export const STUDIES: Study[] = [
                 id: "novel", 
                 trials: [
                     {
-                        id: "bosa", 
+                        id: "virdex", 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/chintro1.mp4"], 
-                        picture: ["../assets/image/cheetahs.jpg"],
-                        sound: ["../assets/audio/ch_1.m4a"]
+                        movie: ["../assets/video/vdintro1.mp4"], 
+                        picture: ["../assets/image/virdexes.jpg"],
+                        sound: ["../assets/audio/vd_1.m4a"]
                     },
                     {
                         id: "tulver", 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/pointro1.mp4"], 
-                        picture: ["../assets/image/porcupines.jpg"],
-                        sound: ["../assets/audio/po_1.m4a"]
+                        movie: ["../assets/video/tvintro1.mp4"], 
+                        picture: ["../assets/image/tulvers.jpg"],
+                        sound: ["../assets/audio/tv_1.m4a"]
                     },
                     {
-                        id: "undo", 
+                        id: "upo", //upos or upo?? 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/kgintro1.mp4"], 
-                        picture: ["../assets/image/kangaroos.jpg"],
-                        sound: ["../assets/audio/kg_1.m4a"]
+                        movie: ["../assets/video/upintro1.mp4"], 
+                        picture: ["../assets/image/upos.jpg"],
+                        sound: ["../assets/audio/up_1.m4a"]
+                    },
+                    {
+                        id: "sibu", 
+                        picture1: [""],
+                        sound1: [""],
+                        movie: ["../assets/video/sbintro1.mp4"], 
+                        picture: ["../assets/image/sibus.jpg"],
+                        sound: ["../assets/audio/sb_1.m4a"]
                     },
                 ]
             }
@@ -96,6 +112,14 @@ export const STUDIES: Study[] = [
                         picture: ["../assets/image/kangaroos.jpg"],
                         sound: ["../assets/audio/kg_2.m4a", "../assets/audio/kg_2_2.m4a"]
                     },
+                    {
+                        id: "skunk", 
+                        picture1: [""],
+                        sound1: [""],
+                        movie: ["../assets/video/skintro1.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_2.m4a", "../assets/audio/sk_2_2.m4a"]
+                    },
                 ]
             },
 
@@ -103,28 +127,36 @@ export const STUDIES: Study[] = [
                 id: "novel", 
                 trials: [
                     {
-                        id: "bosa", 
+                        id: "virdex", 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/chintro1.mp4"], 
-                        picture: ["../assets/image/cheetahs.jpg"],
-                        sound: ["../assets/audio/ch_2.m4a", "../assets/audio/ch_2_2.m4a"]
+                        movie: ["../assets/video/vdintro1.mp4"], 
+                        picture: ["../assets/image/virdexes.jpg"],
+                        sound: ["../assets/audio/vd_2.m4a", "../assets/audio/vd_2_2.m4a"]
                     },
                     {
                         id: "tulver", 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/pointro1.mp4"], 
-                        picture: ["../assets/image/porcupines.jpg"],
-                        sound: ["../assets/audio/po_2.m4a", "../assets/audio/po_2_2.m4a"]
+                        movie: ["../assets/video/tvintro1.mp4"], 
+                        picture: ["../assets/image/tulvers.jpg"],
+                        sound: ["../assets/audio/tv_2.m4a", "../assets/audio/tv_2_2.m4a"]
                     },
                     {
-                        id: "undo", 
+                        id: "upo", //upos or upo?? 
                         picture1: [""],
                         sound1: [""],
-                        movie: ["../assets/video/kgintro1.mp4"], 
-                        picture: ["../assets/image/kangaroos.jpg"],
-                        sound: ["../assets/audio/kg_2.m4a", "../assets/audio/kg_2_2.m4a"]
+                        movie: ["../assets/video/upintro1.mp4"], 
+                        picture: ["../assets/image/upos.jpg"],
+                        sound: ["../assets/audio/up_2.m4a", "../assets/audio/up_2_2.m4a"]
+                    },
+                    {
+                        id: "sibu", 
+                        picture1: [""],
+                        sound1: [""],
+                        movie: ["../assets/video/sbintro1.mp4"], 
+                        picture: ["../assets/image/sibus.jpg"],
+                        sound: ["../assets/audio/sb_2.m4a", "../assets/audio/sb_2_2.m4a"]
                     },
                 ]
             }
@@ -145,6 +177,30 @@ export const STUDIES: Study[] = [
                         picture: ["../assets/image/cheetahs.jpg"],
                         sound: ["../assets/audio/ch_1.m4a"]
                     },
+                    {
+                        id: "porcupine", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/po_3_1.m4a"],
+                        movie: ["../assets/video/po_3_1_Aff.mp4"], 
+                        picture: ["../assets/image/porcupines.jpg"],
+                        sound: ["../assets/audio/po_1.m4a"]
+                    },
+                    {
+                        id: "kangaroo", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/kg_3_1.m4a"],
+                        movie: ["../assets/video/kg_3_1_Aff.mp4"], 
+                        picture: ["../assets/image/kangaroos.jpg"],
+                        sound: ["../assets/audio/kg_1.m4a"]
+                    },
+                    {
+                        id: "skunk", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/sk_3_1.m4a"],
+                        movie: ["../assets/video/sk_3_1_Aff.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_1.m4a"]
+                    },
                 ]
             },
 
@@ -158,6 +214,30 @@ export const STUDIES: Study[] = [
                         movie: ["../assets/video/ch_3_2_Aff.mp4"], 
                         picture: ["../assets/image/cheetahs.jpg"],
                         sound: ["../assets/audio/ch_1.m4a"]
+                    },
+                    {
+                        id: "porcupine", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/po_3_2.m4a"],
+                        movie: ["../assets/video/po_3_2_Aff.mp4"], 
+                        picture: ["../assets/image/porcupines.jpg"],
+                        sound: ["../assets/audio/po_1.m4a"]
+                    },
+                    {
+                        id: "kangaroo", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/kg_3_2.m4a"],
+                        movie: ["../assets/video/kg_3_2_Aff.mp4"], 
+                        picture: ["../assets/image/kangaroos.jpg"],
+                        sound: ["../assets/audio/kg_1.m4a"]
+                    },
+                    {
+                        id: "skunk", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/sk_3_2.m4a"],
+                        movie: ["../assets/video/sk_3_2_Aff.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_1.m4a"]
                     },
                 ]
             },
@@ -173,6 +253,30 @@ export const STUDIES: Study[] = [
                         picture: ["../assets/image/cheetahs.jpg"],
                         sound: ["../assets/audio/ch_1.m4a"]
                     },
+                    {
+                        id: "porcupine", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/po_3_1.m4a"],
+                        movie: ["../assets/video/po_3_1_Corr.mp4"], 
+                        picture: ["../assets/image/porcupines.jpg"],
+                        sound: ["../assets/audio/po_1.m4a"]
+                    },
+                    {
+                        id: "kangaroo", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/kg_3_1.m4a"],
+                        movie: ["../assets/video/kg_3_1_Corr.mp4"], 
+                        picture: ["../assets/image/kangaroos.jpg"],
+                        sound: ["../assets/audio/kg_1.m4a"]
+                    },
+                    {
+                        id: "skunk", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/sk_3_1.m4a"],
+                        movie: ["../assets/video/sk_3_1_Corr.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_1.m4a"]
+                    },
                 ]
             },
 
@@ -186,6 +290,30 @@ export const STUDIES: Study[] = [
                         movie: ["../assets/video/ch_3_2_Corr.mp4"], 
                         picture: ["../assets/image/cheetahs.jpg"],
                         sound: ["../assets/audio/ch_1.m4a"]
+                    },
+                    {
+                        id: "porcupine", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/po_3_2.m4a"],
+                        movie: ["../assets/video/po_3_2_Corr.mp4"], 
+                        picture: ["../assets/image/porcupines.jpg"],
+                        sound: ["../assets/audio/po_1.m4a"]
+                    },
+                    {
+                        id: "kangaroo", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/kg_3_2.m4a"],
+                        movie: ["../assets/video/kg_3_2_Corr.mp4"], 
+                        picture: ["../assets/image/kangaroos.jpg"],
+                        sound: ["../assets/audio/kg_1.m4a"]
+                    },
+                    {
+                        id: "skunk", 
+                        picture1: ["../assets/image/corgi.jpg"],
+                        sound1: ["../assets/audio/sk_3_2.m4a"],
+                        movie: ["../assets/video/sk_3_2_Corr.mp4"], 
+                        picture: ["../assets/image/skunks.jpg"],
+                        sound: ["../assets/audio/sk_1.m4a"]
                     },
                 ]
             }
