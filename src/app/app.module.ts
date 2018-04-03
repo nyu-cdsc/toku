104def69a5986dc9e27ccdf203f1c8dabceefcad
Kelsey Moty <7917220+kelseymoty@users.noreply.github.com>
2018-04-03 00:20:14 -0400

    renamed start page comp; start page animation and on click functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
 import { AgeComponent } from './age/age.component';
-import { StartPageComponent } from './start-page/start-page.component';
+import { StartPageComponent } from './startpage/startpage.component';
 import { StimuliComponent } from './stimuli/stimuli.component';
 import { ResponseService } from './response/response.service';
 
--- src/app/startpage/startpage.component.spec.ts
+++ src/app/startpage/startpage.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StartPageComponent } from './start-page.component';
+import { StartPageComponent } from './startpage.component';
 
 describe('StartPageComponent', () => {
   let component: StartPageComponent;
