c71d012e6c2e013f6be942d35d061f79f8521422
Andrew Kahn <andrew@andrewkahn.me>
2018-07-10 00:02:52 -0400

    adding more components/services and calling in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { SoundComponent } from './stimuli/sound/sound.component';
 import { MovieComponent } from './stimuli/movie/movie.component';
 import { ConditionalComponent } from './stimuli/conditional/conditional.component';
 import { PictureComponent } from './stimuli/picture/picture.component';
+import { ButtonComponent } from './stimuli/button/button.component';
 
 import { ConfigurationService } from './services/configuration/configuration.service';
 import { ResponseService } from './services/response/response.service';
+import { RunnerService } from './services/runner/runner.service';
 import { StimloaderDirective } from './stimloader.directive';
 
 @NgModule({
@@ -18,18 +20,20 @@ import { StimloaderDirective } from './stimloader.directive';
     MovieComponent,
     ConditionalComponent,
     PictureComponent,
-    // ConfigurationService,
-    // ResponseService,
+    ButtonComponent,
     StimloaderDirective
   ],
   entryComponents: [
     PictureComponent,
-    MovieComponent
+    MovieComponent,
+    SoundComponent,
+    ButtonComponent,
+    ConditionalComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [ConfigurationService, ResponseService],
+  providers: [ConfigurationService, ResponseService, RunnerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
