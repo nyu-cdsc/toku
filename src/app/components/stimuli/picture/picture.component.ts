c3719b1d4dc0c5a11e25344eeb8944f016889337
Andrew Kahn <andrew@andrewkahn.me>
2018-09-27 22:37:26 -0400

    coordinates scaling, but not sure I like this method

--- src/app/components/stimuli/picture/picture.component.ts
+++ src/app/components/stimuli/picture/picture.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild, AfterContentInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
 import { Stimuli, Responsive } from '../stimuli';
 import { Message } from '../../../message';
+import { AreaComponent } from '../../responders/area/area.component';
 
 @Component({
   selector: 'app-picture',
   templateUrl: './picture.component.html',
   styleUrls: ['./picture.component.css']
 })
-export class PictureComponent implements Stimuli, Responsive, OnInit {
+export class PictureComponent implements Stimuli, Responsive, OnInit, AfterViewChecked {
   @Input() parameters: any;
   @Output() doneEvent = new EventEmitter<any>();
   @Output() responseEvent = new EventEmitter<Message>();
   @Input() responseEnabled = true; // this can be disabled by parent via [responseEnabled]
+  @ViewChild(AreaComponent) areamap: AreaComponent;
+  @ViewChild('theimage') theimage: ElementRef;
+  // sf = 0.86;
 
   constructor() { }
   ngOnInit() { }
+  ngAfterViewChecked() {
+    this.areamap.scalingFactor = this.getScalingFactor();
+  }
 
   sendMessage(message: Message) {
     console.log('message received in pic! is', message);
@@ -32,4 +39,17 @@ export class PictureComponent implements Stimuli, Responsive, OnInit {
     this.doneEvent.emit(null);
   }
 
+  getScalingFactor() {
+    const width = this.theimage.nativeElement.width;
+    const nwidth = this.theimage.nativeElement.naturalWidth;
+    const height = this.theimage.nativeElement.height;
+    const nheight = this.theimage.nativeElement.naturalHeight;
+    console.log(width, height, nwidth, nheight);
+
+    // TODO ^ make sure I didn't get width/nwidth flipped!
+    // const scalingFactor = Math.min(width / nwidth, height / nheight);
+    const scalingFactor = width / nwidth;
+    console.log('factor ', scalingFactor);
+    return scalingFactor;
+  }
 }
