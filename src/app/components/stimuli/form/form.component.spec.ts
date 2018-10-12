450df6d3dc10bc77ba1e744ff070d12a41b78018
Andrew Kahn <andrew@andrewkahn.me>
2018-10-12 14:10:40 -0400

    fixing test

--- src/app/components/stimuli/form/form.component.spec.ts
+++ src/app/components/stimuli/form/form.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { FormComponent } from './form.component';
 
@@ -8,9 +9,10 @@ describe('FormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      imports: [FormsModule],
+      declarations: [FormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
