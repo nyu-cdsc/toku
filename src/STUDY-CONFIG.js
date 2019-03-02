3f0fe15c16380349bf73184cddb027524139f5f8
Kelsey Moty <7917220+kelseymoty@users.noreply.github.com>
2019-03-01 22:25:38 -0500

    updated simple test congif and sample assets

--- src/STUDY-CONFIG.js
+++ src/STUDY-CONFIG.js
@@ -1,56 +1,56 @@
 ///////// TEMPLATE /////////
 
 ///////// Stimuli /////////
-var exampleStimuli = [
-  // movie where no response is recorded
-  {
-    type: 'action',
-    id: 'frictionIntro',
-    stimuli: [
-      {
-        type: 'movie',
-        parameters: {
-          filename: '../../../assets/movie/1 Study Intro.m4v', // type the location of the movie
-          coordinates: [
-            { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
-            { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
-        }
-      }
-    ]
-  },
-  // movie where a response is recorded
-  {
-    type: 'action',
-    id: 'friction1',
-    stimuli: [
-      {
-        type: 'movie',
-        parameters: {
-          filename: '../../../assets/movie/content/Friction1.mp4', // movie
-          coordinates: [
-            { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
-            { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
-        }
-      }
-    ]
-  },
-  // picture where a response is recorded
-  {
-    type: 'action',
-    id: 'frictionQ1',
-    stimuli: [
-      {
-        type: 'picture',
-        parameters: {
-          filename: '../../../assets/picture/content/Friction1.jpeg',
-          coordinates: [
-            { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
-            { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
-        },
-      }
-    ]
-  },
-];
+// var exampleStimuli = [
+//   // movie where no response is recorded
+//   {
+//     type: 'action',
+//     id: 'frictionIntro',
+//     stimuli: [
+//       {
+//         type: 'movie',
+//         parameters: {
+//           filename: '../../../assets/movie/1 Study Intro.m4v', // type the location of the movie
+//           coordinates: [
+//             { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
+//             { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
+//         }
+//       }
+//     ]
+//   },
+//   // movie where a response is recorded
+//   {
+//     type: 'action',
+//     id: 'friction1',
+//     stimuli: [
+//       {
+//         type: 'movie',
+//         parameters: {
+//           filename: '../../../assets/movie/content/Friction1.mp4', // movie
+//           coordinates: [
+//             { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
+//             { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
+//         }
+//       }
+//     ]
+//   },
+//   // picture where a response is recorded
+//   {
+//     type: 'action',
+//     id: 'frictionQ1',
+//     stimuli: [
+//       {
+//         type: 'picture',
+//         parameters: {
+//           filename: '../../../assets/picture/content/Friction1.jpeg',
+//           coordinates: [
+//             { value: 'left', shape: "rect", coordinates: "120, 56, 370, 728" },
+//             { value: 'right', shape: "rect", coordinates: "672, 48, 926, 732" }],
+//         },
+//       }
+//     ]
+//   },
+// ];
 
 ///////// Sample way to Start Game /////////
 var startGame = [
@@ -61,7 +61,7 @@ var startGame = [
       {
         type: 'picture',
         parameters: {
-          filename: '../../../assets/picture/start.jpeg',
+          filename: '../../../assets/sample/start.jpeg',
           coordinates: [
             { value: 'started', shape: "rect", coordinates: "0, 0, 1024, 768" },
           ]
@@ -76,24 +76,24 @@ var exampleRandom = [
   // first list the stimuli you want to randomize. I've listed two movies.
   {
     type: 'action',
-    id: 'frictionIntro',
+    id: 'item1',
     stimuli: [
       {
         type: 'movie',
         parameters: {
-          filename: '../../../assets/movie/content/intro_to_content_study.mp4' // type the location of the movie
+          filename: '../../../assets/sample/video1.mp4' // type the location of the movie
         }
       }
     ]
   },
   {
     type: 'action',
-    id: 'friction1',
+    id: 'item2',
     stimuli: [
       {
         type: 'movie',
         parameters: {
-          filename: '../../../assets/movie/content/Friction1.mp4', // movie
+          filename: '../../../assets/sample/video2.mp4', // movie
         }
       }
     ]
@@ -109,145 +109,145 @@ var exampleRandom = [
 ///////// Conditionals & Contigent Feedback /////////
 
 // Below is an example of how to give participants contigent feedback,
-var contingentFeedback = [
-  // List the stimuli that you want participants to get contigent feedback from
-  // E.g., In this example, participants watch a movie and give a response on a 6-point scale
-  {
-    type: 'action',
-    id: 'example',
-    stimuli: [
-      {
-        type: 'movie',
-        parameters: {
-          filename: '../../../assets/movie/persistence/wood.mp4',
-          coordinates: [
-            { value: 'one', shape: "rect", coordinates: "161, 646, 250, 740" }, // you'll need these values for later
-            { value: 'two', shape: "rect", coordinates: "282, 646, 370, 740" },
-            { value: 'three', shape: "rect", coordinates: "400, 646, 490, 740" },
-            { value: 'four', shape: "rect", coordinates: "518, 646, 608, 740" },
-            { value: 'five', shape: "rect", coordinates: "634, 646, 726, 740" },
-            { value: 'six', shape: "rect", coordinates: "756, 646, 844, 740" },
-          ],
-        }
-      }
-    ]
-  },
-  // Next, use a 'conditional' object to specify what happens based on the response participants gave.
-  {
-    type: 'conditional',
-    id: 'woodConditional', // this id can be whatever you want
-    items: {
-      // list the response participants gave
-      'one': [{ // this needs to map onto the values from the buttons from above
-        type: 'action',
-        id: 'wood1',
-        // list what you want them to see based on that response
-        // in this case, they are seeing a movie
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_1.mp4',
-            }
-          }
-        ]
-      }],
-      // do a conditional for each response that you want participants to have contingent feedback for
-      'two': [{
-        type: 'action',
-        id: 'wood2',
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_2.mp4',
-            }
-          }
-        ]
-      }],
-      'three': [{
-        type: 'action',
-        id: 'wood3',
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_3.mp4',
-            }
-          }
-        ]
-      }],
-      'four': [{
-        type: 'action',
-        id: 'wood4',
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_4.mp4',
-            }
-          }
-        ]
-      }],
-      'five': [{
-        type: 'action',
-        id: 'wood5',
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_5.mp4',
-            }
-          }
-        ]
-      }],
-      'six': [{
-        type: 'action',
-        id: 'wood6',
-        stimuli: [
-          {
-            type: 'movie',
-            parameters: {
-              filename: '../../../assets/movie/persistence/wood_6.mp4',
-            }
-          }
-        ]
-      }],
-    },
-  },
-];
+// var contingentFeedback = [
+//   // List the stimuli that you want participants to get contigent feedback from
+//   // E.g., In this example, participants watch a movie and give a response on a 6-point scale
+//   {
+//     type: 'action',
+//     id: 'example',
+//     stimuli: [
+//       {
+//         type: 'movie',
+//         parameters: {
+//           filename: '../../../assets/movie/persistence/wood.mp4',
+//           coordinates: [
+//             { value: 'one', shape: "rect", coordinates: "161, 646, 250, 740" }, // you'll need these values for later
+//             { value: 'two', shape: "rect", coordinates: "282, 646, 370, 740" },
+//             { value: 'three', shape: "rect", coordinates: "400, 646, 490, 740" },
+//             { value: 'four', shape: "rect", coordinates: "518, 646, 608, 740" },
+//             { value: 'five', shape: "rect", coordinates: "634, 646, 726, 740" },
+//             { value: 'six', shape: "rect", coordinates: "756, 646, 844, 740" },
+//           ],
+//         }
+//       }
+//     ]
+//   },
+//   // Next, use a 'conditional' object to specify what happens based on the response participants gave.
+//   {
+//     type: 'conditional',
+//     id: 'woodConditional', // this id can be whatever you want
+//     items: {
+//       // list the response participants gave
+//       'one': [{ // this needs to map onto the values from the buttons from above
+//         type: 'action',
+//         id: 'wood1',
+//         // list what you want them to see based on that response
+//         // in this case, they are seeing a movie
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_1.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//       // do a conditional for each response that you want participants to have contingent feedback for
+//       'two': [{
+//         type: 'action',
+//         id: 'wood2',
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_2.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//       'three': [{
+//         type: 'action',
+//         id: 'wood3',
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_3.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//       'four': [{
+//         type: 'action',
+//         id: 'wood4',
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_4.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//       'five': [{
+//         type: 'action',
+//         id: 'wood5',
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_5.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//       'six': [{
+//         type: 'action',
+//         id: 'wood6',
+//         stimuli: [
+//           {
+//             type: 'movie',
+//             parameters: {
+//               filename: '../../../assets/movie/persistence/wood_6.mp4',
+//             }
+//           }
+//         ]
+//       }],
+//     },
+//   },
+// ];
 
 
 // Sometimes you might want a participant to conditionally do different blocks (and not just see a different video or picture).
 // Here's an example of this.
-var conditionalExample = [
-  {
-    type: 'action',
-    id: 'sample',
-    stimuli: [
-      {
-        type: 'movie',
-        parameters: {
-          filename: '../../../assets/movie/persistence/transition_1st.mp4',
-          coordinates: [
-            { value: 'left', shape: "rect", coordinates: "110, 332, 430, 448" },
-            { value: 'right', shape: "rect", coordinates: "642, 244, 912, 518" }],
-        }
-      }
-    ]
-  },
-  {
-    type: 'conditional',
-    id: 'persistence1',
-    items: {
-      'left':
-        block1
-      ,
-      'right':
-        block2
-    }
-  },
-]
+// var conditionalExample = [
+//   {
+//     type: 'action',
+//     id: 'sample',
+//     stimuli: [
+//       {
+//         type: 'movie',
+//         parameters: {
+//           filename: '../../../assets/movie/persistence/transition_1st.mp4',
+//           coordinates: [
+//             { value: 'left', shape: "rect", coordinates: "110, 332, 430, 448" },
+//             { value: 'right', shape: "rect", coordinates: "642, 244, 912, 518" }],
+//         }
+//       }
+//     ]
+//   },
+//   {
+//     type: 'conditional',
+//     id: 'persistence1',
+//     items: {
+//       'left':
+//         block1
+//       ,
+//       'right':
+//         block2
+//     }
+//   },
+// ]
 
 ///////// Forms /////////
 // To make a form, you create an option similar to the one below.
@@ -290,6 +290,7 @@ var forms = {
 
 var Condition1 = [
   // forms,
+  startGame,
   exampleRandom,
   // exampleStimuli,
   // For blocks to be randomly displayed, use the "shuffle" control
@@ -307,11 +308,11 @@ var Condition1 = [
   //   conditionalExample
 ];
 
-var Condition2 = [
-  startGame,
-  contingentFeedback,
-  conditionalExample
-];
+// var Condition2 = [
+//   startGame,
+//   contingentFeedback,
+//   conditionalExample
+// ];
 
 
 ///////// The Study /////////
@@ -324,16 +325,18 @@ export const Project = {
     Condition1,
     // Condition2
   ],
-  name: "Test Study", // Put the name of your study here
+  name: "testStudy", // Put the name of your study here
   ended: { // this what happens when your study ends i.e., the last thing that happens before the study restarts
     // right now, I have it set to play a movie that says "Great job!"
     type: 'action',
-    id: 'done',
+    id: 'form', // you can change the name of this form to whatever
     stimuli: [
       {
-        type: 'movie',
+        type: 'form',
         parameters: {
-          filename: '../../../assets/movie/End.mp4',
+          questions: [
+            { name: 'game', type: "dropdown", options: ["yes", "no"], questionText: "Did you like this game?" },
+          ],
         }
       }
     ]
